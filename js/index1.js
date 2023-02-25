f56a28d622bb831305f644af34a131a3e769c00d
EngrArfin <arfin.cse.green.edu.bd@gmail.com>
2023-02-25 14:47:34 +0600

    add project

---
+++ js/index1.js
@@ -0,0 +1,7 @@
+
+function multiply() {
+    var b = document.getElementById("b").value;
+    var h = document.getElementById("h").value;
+    var result = 0.5 * b * h;
+    document.getElementById("result").innerHTML = " " + result;
+  }
